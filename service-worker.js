2047f5fa665e96d175a792e691fc7a520f8aab7c
jamesy-tech <jamesycreator@gmail.com>
2025-06-11 16:53:16 +1200

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const cacheName = "v1-jamesy";
+const filesToCache = [
+  "/",
+  "/index.html",
+  "/Jamesy Creator/index.html",
+  "/Jamesy Creator/grid.html",
+  "/Jamesy Creator/shapes.png",
+  "/Jamesy Creator/open.html",
+  "/Jamesy Creator/view_md.html"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
